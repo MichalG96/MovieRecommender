f370c0c628fd5fdd30e0b518609835b1633512c0
Michal Grochecki <michalgrochecki.gmail.com>
2020-10-11 00:38:02 +0200

    Created Rating model for storing movie ratings

--- mysite/recommender/models.py
+++ mysite/recommender/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Rating(models.Model):
+    movielens_id = models.IntegerField()    # change to foreign key of Movie
+    who_rated = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    date_rated = models.DateTimeField(default=timezone.now())
+
+
+    def __str__(self):
+        return f'movie_{self.movielens_id}_{self.who_rated}'
