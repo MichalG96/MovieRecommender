c54ebbdd6b9ebba803731c59679d133aa9abd1b9
Michal Grochecki <michalgrochecki.gmail.com>
2020-11-06 21:27:19 +0100

    Added styling for rating filters

--- mysite/recommender/filters.py
+++ mysite/recommender/filters.py
@@ -7,14 +7,13 @@ from django_filters import MultipleChoiceFilter, FilterSet, CharFilter, DateFilt
 from .models import Movie
 
 
+decades_upper = ([1899 + 10 * i for i in range(14)])
+decades_ranges = ['Up to 1899'] + [f"{1900 + 10 * i}'s" for i in range(13)]
+DECADE_CHOICES = (tuple(zip(decades_upper, decades_ranges)))
 
 class MovieFilter(FilterSet):
-    decades_upper = ([1899 + 10 * i for i in range(14)])
-    decades_ranges = ['- 1899'] + [f'{1900 + 10 * i} - {1909 + 10 * i}' for i in range(13)]
-    DECADE_CHOICES = (tuple(zip(decades_upper, decades_ranges)))
 
     year_released = MultipleChoiceFilter(choices=DECADE_CHOICES, widget=forms.CheckboxSelectMultiple(), method='get_movies_from_decades')
-
 
     def get_movies_from_decades(self, queryset, name, value):
         upper_decades_limits = list(map(int, value))
@@ -29,16 +28,11 @@ class MovieFilter(FilterSet):
             'title': ['icontains']
         }
 
-
 class DateInput(forms.DateInput):
     input_type = 'date'
 
 class RatingFilter(FilterSet):
     movie__title = CharFilter(lookup_expr='icontains')
-
-    decades_upper = ([1889 + 10 * i for i in range(15)])
-    decades_ranges = ['- 1889'] + [f'{1890 + 10 * i} - {1899 + 10 * i}' for i in range(14)]
-    DECADE_CHOICES = (tuple(zip(decades_upper, decades_ranges)))
 
     possible_ratings = [i for i in range(1, 11)]
     RATING_CHOICES = (tuple(zip(possible_ratings, possible_ratings)))
@@ -63,10 +57,7 @@ class RatingFilter(FilterSet):
         return movies_from_decades
 
     # class Meta:
-    #
-    #     form = {
-    #         'input_type': 'date'
-    #     }
+
     #     model = Movie
     #     fields = {
     #         'movie__title': ['icontains']
