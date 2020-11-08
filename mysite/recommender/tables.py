07b6126b531413e59fd217aeca262189c1ec3894
Michal Grochecki <michalgrochecki.gmail.com>
2020-11-08 18:55:56 +0100

    Added styling for forms

--- mysite/recommender/tables.py
+++ mysite/recommender/tables.py
@@ -9,16 +9,16 @@ class RatingsTable(tables.Table):
                               accessor='movie.title',
                               attrs={
                                     "a": {"class": "link-glow"},
-                                    "td": {"class": "link-glow"}
+                                    "td": {"class": "link-glow"},
+                                    "th": {"style": "width: 22%"}
                               })
-    director = tables.Column(accessor='movie.director')
-    year_released = tables.Column(accessor='movie.year_released', attrs={"th": {"class": "center-column"}})
+    director = tables.Column(accessor='movie.director', attrs={"td": {"class": "row-glow"}, "th": {"style": "width: 13%"}})
+    year_released = tables.Column(accessor='movie.year_released', attrs={"td": {"class": "row-glow center-column"}, "th": {"class": "center-column", "style": "width: 9%"}})
     movielens_id = tables.Column(accessor='movie.movielens_id', attrs={"td": {"class": "row-glow center-column"},
-                                                                       "th": {"class": "center-column"}})
-    date_rated = tables.DateTimeColumn(format ='d/m/Y H:i:s', attrs={"th": {"class": "center-column"}})
-    value = tables.Column(attrs={"td": {"class": "bold-row"},
-                                 "th": {"class": "center-column"}
-                                 })
+                                                                       "th": {"class": "center-column", "style": "width: 14%"}})
+    date_rated = tables.DateTimeColumn(format ='d/m/Y H:i:s', attrs={"td": {"class": "row-glow center-column"}, "th": {"class": "center-column", "style": "width: 16%"}})
+    value = tables.Column(attrs={"td": {"class": "row-glow center-column bold-col"},
+                                 "th": {"class": "center-column" , "style": "width: 10%"}})
 
 
     class Meta:
@@ -34,14 +34,15 @@ class RatingsTable(tables.Table):
 class MoviesTable(tables.Table):
     # TODO: add custom styling for this column, and for header
 
-    id = tables.Column(attrs={"th":{"class": "center-column", "style": "width: 7%"}})
-    movielens_id = tables.Column(attrs={"th": {"class": "center-column", "style": "width: 15%"}})
-    imdb_id = tables.Column(attrs={"th":{"class": "center-column", "style": "width: 11%"}})
-    tmdb_id = tables.Column(attrs={"th":{"class": "center-column", "style": "width: 12%"}})
+    id = tables.Column(attrs={"td": {"class": "row-glow center-column"}, "th":{"class": "center-column", "style": "width: 7%"}})
+    movielens_id = tables.Column(attrs={"td": {"class": "row-glow center-column"}, "th": {"class": "center-column", "style": "width: 15%"}})
+    imdb_id = tables.Column(attrs={"td": {"class": "row-glow center-column"}, "th":{"class": "center-column", "style": "width: 11%"}})
+    tmdb_id = tables.Column(attrs={"td": {"class": "row-glow center-column"}, "th":{"class": "center-column", "style": "width: 12%"}})
     title = tables.LinkColumn("movie_detail", args=[A("pk")], attrs={"a": {"class": "link-glow"},
+                                                                     "td": {"class": "link-glow"},
                                                                      "th": {"style": "width: 28%"}})
-    year_released = tables.Column(attrs={"th": {"class": "center-column", "style": "width: 8%"}})
-    director = tables.Column(attrs={"th": {"style": "width: 13%"}})
+    year_released = tables.Column(attrs={"td": {"class": "row-glow center-column"}, "th": {"class": "center-column", "style": "width: 8%"}})
+    director = tables.Column(attrs={"td": {"class": "row-glow"}, "th": {"style": "width: 13%"}})
 
     class Meta:
         model = Movie
