cf3abf5d0b6843b6e275187a73aa23217a99322d
Michal Grochecki <michalgrochecki@gmail.com>
2021-01-15 22:55:06 +0100

    Adjusted button placement in RatingView on mobile devices

--- mysite/recommender/static/recommender/main.js
+++ mysite/recommender/static/recommender/main.js
@@ -1,8 +1,8 @@
-document.querySelectorAll(".decades-container label").forEach((el) => {
+document.querySelectorAll(".form-container label").forEach((el) => {
     el.className = "plus";
 });
 
-document.querySelectorAll(".decades-container label input").forEach((checkbox) => {
+document.querySelectorAll(".form-container label input").forEach((checkbox) => {
     if (checkbox.checked) {
         checkbox.parentElement.className = "tick";
         checkbox.parentElement.style.cssText = "color: gold;border: 0.1rem solid gold;";
@@ -10,7 +10,7 @@ document.querySelectorAll(".decades-container label input").forEach((checkbox) =
 });
 
 
-document.querySelectorAll(".decades-container label input").forEach((checkbox) => {
+document.querySelectorAll(".form-container label input").forEach((checkbox) => {
     checkbox.addEventListener("change", (event) => {
         if (event.target.checked) {
             checkbox.parentElement.className = "tick";
